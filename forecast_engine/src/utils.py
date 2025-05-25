0b9667adfd7fae8587ca7edc9bed718c328e8741
Behzad Rowshanravan <behr87@hotmail.com>
2025-05-25 08:21:50 +0000

    initial app code

--- forecast_engine/src/utils.py
+++ forecast_engine/src/utils.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
+
+def read_csv_file(file_path):
+    df= pd.read_csv(file_path)
+    return df.dropna(how="any", axis="rows")
+
+
+
 def timeseries_format_validity_checker(required_format, dataframe, date_column):
     try:
         pd.to_datetime(dataframe[date_column], format=required_format)
@@ -8,3 +15,12 @@ def timeseries_format_validity_checker(required_format, dataframe, date_column):
         date_format_validity_status= False
     
     return date_format_validity_status
+
+def timeseries_date_granularity_validity_checker(stated_granularity, dataframe):
+    try:
+        dataframe.resample(stated_granularity).mean()
+        data_granularity_validity_status= True
+    except:
+        data_granularity_validity_status= False
+    
+    return data_granularity_validity_status
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from forecast_engine.src.utils import (
-    timeseries_format_validity_checker
+    read_csv_file,
+    timeseries_format_validity_checker,
+    timeseries_date_granularity_validity_checker,
     )
 
 import pandas as pd
@@ -8,15 +10,15 @@ from prophet import Prophet
 import streamlit as st
 import os
 
-DATA_DIRECTORY_PATH= "/workspaces/data/"
-FILE_NAME= [
-    "stationary_sales_data_date_valid.csv",
-    "non_stationary_sales_data_date_valid.csv", 
-    "non_stationary_sales_data_date_invalid.csv",
-    ][1]
-TIME_SERIES_COLUM_NAME= "date_time"
+# DATA_DIRECTORY_PATH= "/workspaces/data/"
+# FILE_NAME= [
+#     "stationary_sales_data_date_valid.csv",
+#     "non_stationary_sales_data_date_valid.csv", 
+#     "non_stationary_sales_data_date_invalid.csv",
+#     ][1]
+# TIME_SERIES_COLUM_NAME= "date_time"
 NEW_TIME_SERIES_COLUM_NAME= "ds"
-DATA_COLUM_NAME= "sales"
+# DATA_COLUM_NAME= "sales"
 NEW_DATA_COLUM_NAME= "y"
 REQUIRED_TIMESERIES_DATA_DATE_FORMAT= "%d-%m-%Y"
 DATA_GRANULARITY_OPTIONS={
@@ -24,7 +26,7 @@ DATA_GRANULARITY_OPTIONS={
     "weekly": "W",
     "monthly": "M"
 }
-DATA_GRANULARITY= "daily"
+# DATA_GRANULARITY= "daily"
 PREDICTION_PERIOD=30
 EXPECTED_ONE_FULL_CYCLE_PERIOD= 60
 
@@ -37,44 +39,101 @@ st.header("")
 
 
 
+uploaded_file = st.file_uploader(
+    label="Choose a CSV file", 
+    accept_multiple_files=False
+)
 
-
-
-df= pd.read_csv(DATA_DIRECTORY_PATH+FILE_NAME)
-df= df.dropna(how="any", axis="rows")
-df= df.rename(columns={
-    TIME_SERIES_COLUM_NAME: NEW_TIME_SERIES_COLUM_NAME, 
-    DATA_COLUM_NAME: NEW_DATA_COLUM_NAME
-    })
-
-date_format_validity_status= timeseries_format_validity_checker(
-    required_format= REQUIRED_TIMESERIES_DATA_DATE_FORMAT, 
-    dataframe=df, 
-    date_column= NEW_TIME_SERIES_COLUM_NAME
-    )
-
-
-if date_format_validity_status:
-    df[NEW_TIME_SERIES_COLUM_NAME]= pd.to_datetime(
-        df[NEW_TIME_SERIES_COLUM_NAME], 
-        format=REQUIRED_TIMESERIES_DATA_DATE_FORMAT
+if uploaded_file:
+    df= read_csv_file(file_path= uploaded_file)
+    st.write("Please select the date column. The date MUST be in the `DD-MM-YYYY` format")
+    TIME_SERIES_COLUM_NAME= st.multiselect(
+        label= "Please select the date column. The date MUST be in the `DD-MM-YYYY` format",
+        options= df.columns.tolist(),
+        placeholder= "Date column ...",
+        default=None
         )
+    
+    if TIME_SERIES_COLUM_NAME != []:
+        date_format_validity_status= timeseries_format_validity_checker(
+            required_format= REQUIRED_TIMESERIES_DATA_DATE_FORMAT, 
+            dataframe=df, 
+            date_column= TIME_SERIES_COLUM_NAME,
+            )
 
-    df= df.set_index(NEW_TIME_SERIES_COLUM_NAME)
-    df= df.resample(DATA_GRANULARITY_OPTIONS[DATA_GRANULARITY]).mean()
-    df= df.reset_index(drop=False)
-    df= df.sort_values(by=NEW_TIME_SERIES_COLUM_NAME, ascending=True)
+        if date_format_validity_status and TIME_SERIES_COLUM_NAME != []:
+            st.write("Please select the target column you wish to predict")
+            DATA_COLUM_NAME= st.multiselect(
+                label= "Please select the target column you wish to predict",
+                options= df.columns.tolist().remove(TIME_SERIES_COLUM_NAME),
+                placeholder= "Target column ...",
+                default=None
+                )
+            
+            if DATA_COLUM_NAME != [] and TIME_SERIES_COLUM_NAME != []:
+                df= df[[DATA_COLUM_NAME, TIME_SERIES_COLUM_NAME]]
+                df= df.rename(columns={
+                    TIME_SERIES_COLUM_NAME: NEW_TIME_SERIES_COLUM_NAME, 
+                    DATA_COLUM_NAME: NEW_DATA_COLUM_NAME
+                    })
+                df[NEW_TIME_SERIES_COLUM_NAME]= pd.to_datetime(
+                    df[NEW_TIME_SERIES_COLUM_NAME], 
+                    format=REQUIRED_TIMESERIES_DATA_DATE_FORMAT
+                    )
+                df= df.set_index(NEW_TIME_SERIES_COLUM_NAME)
+                
+                st.write("Please select the granularity/resolution of your data")
+                DATA_GRANULARITY = st.selectbox(
+                    label= "Please select the granularity/resolution of your data",
+                    options= list(DATA_GRANULARITY_OPTIONS.keys()),
+                    index=None,
+                    placeholder="Data granularity...",
+                )
+                
+                if DATA_GRANULARITY != []:
+                    data_granularity_validity_status= timeseries_date_granularity_validity_checker(
+                        stated_granularity= DATA_GRANULARITY_OPTIONS[DATA_GRANULARITY], 
+                        dataframe=df
+                        )
+                    if data_granularity_validity_status:
+                        df= df.resample(DATA_GRANULARITY_OPTIONS[DATA_GRANULARITY]).mean()
+                        df= df.reset_index(drop=False)
+                        df= df.sort_values(by=NEW_TIME_SERIES_COLUM_NAME, ascending=True)
+                        print(df.head())
+                    else:
+                        st.write("There are missing data points for the selected granularity. Please select another option.")
 
-    clf= Prophet()
-    clf.add_seasonality(
-        name=DATA_GRANULARITY_OPTIONS[DATA_GRANULARITY], 
-        period=EXPECTED_ONE_FULL_CYCLE_PERIOD, 
-        fourier_order=5
-        )
-    clf.fit(df)
-    future = clf.make_future_dataframe(periods=PREDICTION_PERIOD, freq=DATA_GRANULARITY_OPTIONS[DATA_GRANULARITY])
-    forecast = clf.predict(future)
+        else:
+            st.write("The date column is not in the `DD-MM-YYYY` required format. Please upload a correctly formatted file.")
+        
+            
 
-    clf.plot(forecast, xlabel= TIME_SERIES_COLUM_NAME, ylabel= DATA_COLUM_NAME)
-    plt.tight_layout()
-    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+# df= df.set_index(NEW_TIME_SERIES_COLUM_NAME)
+# df= df.resample(DATA_GRANULARITY_OPTIONS[DATA_GRANULARITY]).mean()
+# df= df.reset_index(drop=False)
+# df= df.sort_values(by=NEW_TIME_SERIES_COLUM_NAME, ascending=True)
+
+# clf= Prophet()
+# clf.add_seasonality(
+#     name=DATA_GRANULARITY_OPTIONS[DATA_GRANULARITY], 
+#     period=EXPECTED_ONE_FULL_CYCLE_PERIOD, 
+#     fourier_order=5
+#     )
+# clf.fit(df)
+# future = clf.make_future_dataframe(periods=PREDICTION_PERIOD, freq=DATA_GRANULARITY_OPTIONS[DATA_GRANULARITY])
+# forecast = clf.predict(future)
+
+# clf.plot(forecast, xlabel= TIME_SERIES_COLUM_NAME, ylabel= DATA_COLUM_NAME)
+# plt.tight_layout()
+# plt.show()
